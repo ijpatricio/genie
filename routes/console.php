dac53510edb1c1bc1f7201c9dbcde4eb989357d8
Joao Patricio <ijpatricio@gmail.com>
2024-05-24 15:45:27 +0100

    wip

--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('app:new filament-issue')->at('00:05');
-Schedule::command('app:package filament-issue')->at('00:10');
+Schedule::command('app:new filament-issue')->dailyAt('00:05');
+Schedule::command('app:package filament-issue')->dailyAt('00:10');
 
-Schedule::command('app:new filament-issue')->at('14:30');
-Schedule::command('app:package filament-issue')->at('14:35');
+Schedule::command('app:new filament-issue')->dailyAt('14:50');
+Schedule::command('app:package filament-issue')->dailyAt('14:55');
